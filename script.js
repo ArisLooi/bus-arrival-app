da7fcad50f8ada01b530663ea49ee6e8aa25a179
Aris <arislooi@outlook.com>
2024-11-26 18:16:58 +0800

    Create a formatArrivalData function

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ async function fetchBusArrival(busStopId) {
         throw new Error('Error fetching bus arrival data.');
     }
 }
+
+function formatArrivalData(arrivalData) {
+    const buses = arrivalData.services;
+    const formattedData = [];
+    for (const bus of buses) {
+        const arrivalTimerString = `${bus.next_bus_mins} mins(s)`;
+        formattedData.push(`
+            <div>
+                <strong>Bus ${bus.bus_no}</strong>: ${arrivalTimeString}
+            </div>
+        `)
+        }
+    return formattedData.join("");
+}
