9dfd97dd7fa147f7f0e5688a1cd3f656c2cda755
Aris <arislooi@outlook.com>
2024-11-26 18:00:10 +0800

    Create an asynchronous fetchBusArrival function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const busStopIdInput = document.getElementById("busStopId");
 const arrivalInfo = document.getElementById("arrivalInfo");
 
 async function fetchBusArrival(busStopId) {
-    const response = await fetch(`https://sg-bus-arrivals.vercel.app/?id=`);
+    const response = await fetch(`https://sg-bus-arrivals.vercel.app/?id=${busStopId}`);
     if (response.ok) {
     const data = await response.json();
     return data;
